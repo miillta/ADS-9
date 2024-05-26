7bf83a1a6c6563ff74085d49b282b78e7766c4a0
MilanaTarasova <150841920+miillta@users.noreply.github.com>
2024-05-26 17:35:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,23 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+// открываем файл на чтение
+  std::ifstream file(filename);
+// если файл не открылся, прекращаем работу
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+  }
+  char symbol;
+  std::string word;
+  BST<std::string> tree;
+  while (file.get(symbol)) {
+    if (isalpha(symbol)) { //определяет латинская буква ли символ
+      word += tolower(symbol); // функция tolower выполняет преобразование прописных букв в строчные
+    } else if (!word.empty()) {
+      tree.add(word);
+      word.clear();
+    }
+  }
+  file.close();
+  return tree;
 }
